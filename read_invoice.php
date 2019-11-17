51147f6284dc0e89c18626a7f4ca2634c49e7bed
Felix <felixhlw@gmail.com>
2019-11-17 12:05:25 +0800

    Update read_invoice.php

--- read_invoice.php
+++ read_invoice.php
@@ -5,9 +5,9 @@ $rows= fopen("invoice.csv","r");
 
 $data = fgetcsv($rows);
 
-$sql="insert into invoice(year,period,code,number,expend,type) values('$data[0]','$data[1]','$data[2]','$data[3]','$data[4]','$data[5]')";
+$sql="insert into invoice(id,year,period,code,number,expend,type) values('$data[0]','$data[1]','$data[2]','$data[3]','$data[4]','$data[5]'','$data[5]'','$data[5]','$data[6]')";
 
-while (($data = fgetcsv($rows, 1000, ",")) !== FALSE) {
+while (($data = fgetcsv($rows, 10000, ",")) !== FALSE) {
     $pdo->exec($sql);
 }
 
